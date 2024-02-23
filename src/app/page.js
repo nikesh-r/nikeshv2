3735bac281e97ef251a627d935fe056b581527d7
Nikesh <nikesh.ramnani@gmail.com>
2024-02-23 15:50:58 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            updatw1{" "}
+            updatw2{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
